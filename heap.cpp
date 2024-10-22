b310675d6555d3611f9dd9c4481b6aa4d461415f
ishan2718 <168367178+ishan2718@users.noreply.github.com>
2024-10-22 20:09:25 +0000

    insertElement, findMax, deleteMin working good.

--- heap.cpp
+++ heap.cpp
@@ -2,14 +2,14 @@
 
 void minHeap::buildHeap(string input)
 {
-    curr_length = 0;
+    curr_size = 0;
     vector<int> element_list = string_to_list(input);
     for (int i = 0; i < element_list.size(); i++)
     {
-        curr_length++;
-        heap[curr_length] = element_list[i];
+        curr_size++;
+        heap[curr_size] = element_list[i];
     }
-    for (int i = curr_length / 2; i > 0; i--)
+    for (int i = curr_size / 2; i > 0; i--)
     {
         compare_parent_child(i);
     }
@@ -17,46 +17,51 @@ void minHeap::buildHeap(string input)
 
 void minHeap::compare_parent_child(int parent_index)
 {
-    if (parent_index <= curr_length / 2)
+    if (parent_index <= curr_size / 2)
     {
         int child_index = 2 * parent_index;
-        if ((child_index < curr_length) && (curr_length % 2 == 1) && (heap[2 * parent_index + 1] < heap[2 * parent_index]))
+        if ((child_index < curr_size) && (curr_size % 2 == 1) && (heap[2 * parent_index + 1] < heap[2 * parent_index]))
         {
             child_index = 2 * parent_index + 1;
         }
         if (heap[child_index] < heap[parent_index])
         {
-            swapElements(child_index, parent_index);
+            swapElements(parent_index, child_index);
             compare_parent_child(child_index);
         }
     }
 }
 
-void minHeap::swapElements(int index1, int index2)
+void minHeap::swapElements(int parent_index, int child_index)
 {
-    int temp = heap[index1];
-    heap[index1] = heap[index2];
-    heap[index2] = temp;
+    int temp = heap[parent_index];
+    heap[parent_index] = heap[child_index];
+    heap[child_index] = temp;
 }
 
 void minHeap::printHeap()
 {
-    for (int i = 1; i <= curr_length; i++)
+    for (int i = 1; i <= curr_size; i++)
     {
         cout << heap[i] << " ";
     }
     cout << endl;
 }
 
-void minHeap::insertElement(int new_element)
+void minHeap::insertElement(const int new_element)
 {
-    curr_length++;
-    int new_element_index = curr_length;
-    heap[new_element_index] = new_element;
-    while (heap[new_element_index / 2] > heap[new_element_index])
+    if (isFull())
     {
-        swapElements(new_element_index, new_element_index / 2);
-        new_element_index /= 2;
+        cout << "Heap Overflow" << endl;
+    }
+    else
+    {
+        int hole = ++curr_size;
+        for (; hole > 1 && new_element < heap[hole / 2]; hole /= 2)
+        {
+            heap[hole] = heap[hole / 2];
+        }
+        heap[hole] = new_element;
     }
 }
 
@@ -65,14 +70,85 @@ int minHeap::findMin()
     return heap[1];
 }
 
-// int minHeap::findMax(int index)
-// {
-//     if (2 * index + 1 <= curr_length)
-//     {
-//         findMax(max(2 * index, 2 * index + 1));
-//     }
-//     return heap[index];
-// }
+void minHeap::deleteMin()
+{
+    int hole = 1;
+    int last_element = heap[curr_size];
+    while (hole <= curr_size / 2)
+    {
+        int smaller_child_index = getSmallerChild(hole);
+        swapElements(hole, smaller_child_index);
+        hole = smaller_child_index;
+    }
+    if (hole != curr_size)
+    {
+        swapElements(hole, curr_size);
+    }
+    curr_size--;
+}
+
+int minHeap::getElement(int index)
+{
+    return heap[index];
+}
+
+int minHeap::getSmallerChild(int parent_index)
+{
+    int left_child_index = 2 * parent_index;
+    int right_child_index = 2 * parent_index + 1;
+    if (left_child_index <= curr_size)
+    {
+        if (right_child_index <= curr_size)
+        {
+            if (heap[left_child_index] < heap[right_child_index])
+            {
+                return left_child_index;
+            }
+            return right_child_index;
+        }
+        return left_child_index;
+    }
+    return parent_index;
+}
+
+int minHeap::getLargerChild(int parent_index)
+{
+    int left_child_index = 2 * parent_index;
+    int right_child_index = 2 * parent_index + 1;
+    if (left_child_index <= curr_size)
+    {
+        if (right_child_index <= curr_size)
+        {
+            if (heap[left_child_index] < heap[right_child_index])
+            {
+                return right_child_index;
+            }
+            return left_child_index;
+        }
+        return left_child_index;
+    }
+    return parent_index;
+}
+
+int minHeap::findMax()
+{
+    int mark = 1;
+    while (mark <= curr_size / 2)
+    {
+        int larger_child_index = getLargerChild(mark);
+        if (mark == larger_child_index)
+        {
+            return heap[mark];
+        }
+        mark = larger_child_index;
+    }
+    return heap[mark];
+}
+
+bool minHeap::isFull()
+{
+    return (curr_size == MAX_LENGTH);
+}
 
 vector<int> string_to_list(string s)
 {
--- heap.h
+++ heap.h
@@ -11,15 +11,21 @@ class minHeap
 {
 private:
     int heap[MAX_LENGTH];
-    int curr_length;
+    int curr_size;
+
 public:
+    int getElement(int);
     void buildHeap(string);
     void printHeap();
     void swapElements(int, int);
     void compare_parent_child(int);
     void insertElement(int);
     int findMin();
-    int findMax(int);
+    int findMax();
+    bool isFull();
+    void deleteMin();
+    int getSmallerChild(int);
+    int getLargerChild(int);
 };
 
 // vector functions
