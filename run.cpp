bd6e79f107ccc4154229aab6c4863445c31b1d86
ishan2718 <168367178+ishan2718@users.noreply.github.com>
2024-10-22 21:36:53 +0000

    made templates. all done now.

--- run.cpp
+++ run.cpp
@@ -4,8 +4,11 @@ int main()
 {
     string input;
     getline(cin, input);
-    minHeap my_heap;
+    minHeap<int> my_heap;
     my_heap.buildHeap(input);
+    my_heap.printHeap();
+    cout << my_heap.findMax()<<endl;
+    my_heap.deleteMin();
     my_heap.printHeap();
     return 0;
 }
